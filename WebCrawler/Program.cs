2174842b63b8d9858d7d15000382b7d62256bb89
lester.wei <lunyi.lester@gmail.com>
2019-10-05 10:04:44 +0800

    refactor

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -1,47 +1,114 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+﻿using ConsoleApp.Models;
+using HtmlAgilityPack;
 using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace WebCrawler
+namespace ConsoleApp
 {
     class Program
     {
-        static IWebDriver _driver;
-
-        static string LoginUrl = "https://statementdog.com/users/sign_in";
-        static string HealthCheckUrl = "https://statementdog.com/analysis/tpe/6153/stock-health-check";
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Initial();
+            //Update Model
+            //Scaffold-DbContext "Server=localhost;Database=StockDb;User ID=sa;Password=sa;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Force -UseDatabaseNames
 
-            Complete();
+            //var context = new StockDbContext();
+
+            //var stock = new Stocks()
+            //{
+            //};
+
+
+
+            string url = "http://5850web.moneydj.com/z/zc/zcx/zcxNew_2498.djhtm";
+            string stockDepositUrl = "https://statementdog.com/analysis/tpe/6153";
+
+            string stockHealthCheckUrl = "https://statementdog.com/analysis/tpe/6153/stock-health-check";
+
+            var rootNode = GetRootNoteByUrl(stockHealthCheckUrl);
+
+            var ss = rootNode.SelectSingleNode(@"/html/body");
+
+            var ss1 = rootNode.SelectNodes(@"//div[contains(@class, 'stock-health-check-module-score-description')]");
+
+            for (int i = 0; i < ss1.Count; i++)
+            {
+                var s = ss1[i].InnerHtml;
+            }
+
+            ////*[@id="stock-health-result"]/div[2]
+            /////html/body/div[2]/div[3]/div/div[2]/div[2]
+            //context.Stocks.Add(stock);
+            //await context.SaveChangesAsync();
+
+            //Console.ReadLine();
         }
 
-        static void Initial()
-        {
-            ChromeOptions chromeBrowserOptions = new ChromeOptions();
-            //需要阻擋跳出視窗時，可將下面註解移除
-            chromeBrowserOptions.AddArgument("--disable-popup-blocking");
-            //要節省流量，不載入圖片的時候，可將下面註解移除
-            //chromeBrowserOptions.AddExtension(@"ChromeDriver\Block-image_v1.1.crx"); // 載入阻擋圖片外掛程式
-            _driver = new ChromeDriver(@"..\..\..\..\BrowserPath", chromeBrowserOptions);
-            //可避免網頁被cache住，一直查到舊資料(需不斷重複查詢同一網頁時就會用到)
-            _driver.Manage().Cookies.DeleteAllCookies();
-            _driver.Navigate().GoToUrl(LoginUrl);
-            var emailElement = _driver.FindElement(By.Id("user_email"));
-            var passwordElement = _driver.FindElement(By.Id("user_password"));
-            var submitElement = _driver.FindElement(By.ClassName("submit-btn"));
+        static Dictionary<string, dynamic> map = new Dictionary<string, dynamic> {
+            { "big5", "big5" },
+            { "utf-8", 65001 },
+        };
 
-            emailElement.SendKeys("lunyi.lester@gmail.com");
-            passwordElement.SendKeys("1q2w3e4r");
-            submitElement.Click();
+        static HtmlNode GetRootNoteByUrl(string url, bool isUtf8 = true)
+        {
+            var web = new HtmlWeb();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            Encoding webEncode = isUtf8 ? Encoding.GetEncoding(65001) : Encoding.GetEncoding("big5");
+            web.OverrideEncoding = webEncode;
+
+            var doc =  web.Load(url);
+            return doc.DocumentNode;
         }
 
-        static void Complete()
+        static void ParserFinance(HtmlDocument doc)
         {
-            _driver.Close();
-            _driver.Dispose();
+
+            //var node = doc.DocumentNode.SelectSingleNode(@"/html/body/div/table/tbody/tr[2]/td[2]/center/table[1]/tbody/tr/td/table[2]/tbody/tr[2]/td[2]");
+            var node = doc.DocumentNode.SelectSingleNode(@"/html/body/div/table/tr[2]/td[2]/table[1]/tr/td/table[2]");
+
+            var open = node.ChildNodes[3].ChildNodes[3].InnerText;
+            var high = node.ChildNodes[3].ChildNodes[7].InnerText;
+            var low = node.ChildNodes[3].ChildNodes[11].InnerText;
+            var close = node.ChildNodes[3].ChildNodes[15].InnerText;
+
+            //var dateString = "/html/body/div/table/tr[2]/td[2]/table[1]/tr/td/table[1]/tr[2]/td/div/div[1]/div[2]/div[1]/div[1]/div[1]/span[1]";
+            var dateString = "/html/body/div/table/tr[2]/td[2]/table[1]/tr/td/table[1]/tr[2]/td";
+
+            var dateNode = doc.DocumentNode.SelectSingleNode(dateString);
+
+
+
+            var node1 = doc.DocumentNode.SelectSingleNode(@"/html/body/div/table/tr[2]/td[2]/table[1]/tr/td/table[2]/tr[2]/td[2]");
+
+            var vvv = node.InnerText;
+            Console.WriteLine(open);
+        }
+
+        static void ParserBasic(HtmlDocument doc) 
+        {
+
+            //var node = doc.DocumentNode.SelectSingleNode(@"/html/body/div/table/tbody/tr[2]/td[2]/center/table[1]/tbody/tr/td/table[2]/tbody/tr[2]/td[2]");
+            var node = doc.DocumentNode.SelectSingleNode(@"/html/body/div/table/tr[2]/td[2]/table[1]/tr/td/table[2]");
+
+            var open = node.ChildNodes[3].ChildNodes[3].InnerText;
+            var high = node.ChildNodes[3].ChildNodes[7].InnerText;
+            var low = node.ChildNodes[3].ChildNodes[11].InnerText;
+            var close = node.ChildNodes[3].ChildNodes[15].InnerText;
+
+            //var dateString = "/html/body/div/table/tr[2]/td[2]/table[1]/tr/td/table[1]/tr[2]/td/div/div[1]/div[2]/div[1]/div[1]/div[1]/span[1]";
+            var dateString = "/html/body/div/table/tr[2]/td[2]/table[1]/tr/td/table[1]/tr[2]/td";
+
+            var dateNode = doc.DocumentNode.SelectSingleNode(dateString);
+
+
+
+            var node1 = doc.DocumentNode.SelectSingleNode(@"/html/body/div/table/tr[2]/td[2]/table[1]/tr/td/table[2]/tr[2]/td[2]");
+
+            var vvv = node.InnerText;
+            Console.WriteLine(open);
         }
     }
 }
