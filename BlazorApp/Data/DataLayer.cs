3636be90cd35bf6908892950000b1c9ab4387e48
lester.wei <lunyi.lester@gmail.com>
2019-10-08 18:58:38 +0800

    for test blazor

---
+++ BlazorApp/Data/DataLayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataService.Models;
+
+namespace BlazorApp.Data
+{
+    public interface IDataLayer
+    {
+        Stocks[] GetStocks();
+    }
+    public class DataLayer : IDataLayer
+    {
+        Stocks[] IDataLayer.GetStocks()
+        {
+            var context = new StockDbContext();
+            return context.Stocks.ToArray();
+        }
+    }
+}
---
+++ BlazorApp/Model/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp.Model
+{
+    public class Todo
+    {
+        public string Text { get; set; }
+        public bool Done { get; set; }
+    }
+}
