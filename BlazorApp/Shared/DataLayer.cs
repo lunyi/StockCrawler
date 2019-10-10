390c2229484a7cdc4fe4d5889b830103b631ce8b
lester.wei <lunyi.lester@gmail.com>
2019-10-10 15:20:35 +0800

    add StockLayout.razor

--- BlazorApp/Shared/DataLayer.cs
+++ BlazorApp/Shared/DataLayer.cs
@@ -11,10 +11,29 @@ namespace BlazorApp.Shared
     }
     public class DataLayer : IDataLayer
     {
+        string[] NotContainStocks = new string[] {
+                "0050",
+                "0051",
+                "0052",
+                "0053",
+                "0054",
+                "0055",
+                "0056",
+                "0057",
+                "0058",
+                "0059",
+                "0060",
+                "0061"
+            };
+
         Task<Stocks[]> IDataLayer.GetStocksAsync()
         {
             var context = new StockDbContext();
-            return context.Stocks.OrderBy(p=>p.StockId).ToArrayAsync();
+
+            return context.Stocks
+                .Where(p => !NotContainStocks.Contains(p.StockId))
+                .OrderBy(p=>p.StockId)
+                .ToArrayAsync();
         }
     }
 }
