42e94388d1bf4ea670e2f39be22fec9e87a83f72
lester.wei <lunyi.lester@gmail.com>
2019-12-19 07:47:53 +0800

    change domain name

--- BlazorApp/wwwroot/js/price.js
+++ BlazorApp/wwwroot/js/price.js
@@ -1,7 +1,7 @@
 ﻿(function () {
 
     var stockId = getUrlParameter('stockId');
-    var url = "http://220.133.185.1:8081/stock/" + getUrlParameter('stockId');
+    var url = "http://" + window.location.hostname + ":8081/stock/" + getUrlParameter('stockId');
     //var url = "https://localhost:44367/stock/" + stockId;
     var kUrl = "https://so.cnyes.com/JavascriptGraphic/chartstudy.aspx?country=tw&market=tw&code=" + stockId + "&divwidth=550&divheight=330";
 
