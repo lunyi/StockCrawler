d42c07c460418c5ec5ce7a558ac59fd4071bb0cc
lester.wei <lester.wei@alliance.tw>
2020-04-08 21:08:43 +0800

    refactor

--- WebCrawler/CnyParser.cs
+++ WebCrawler/CnyParser.cs
@@ -33,12 +33,18 @@ namespace WebCrawler
             }
 
             var dd = await context.Prices.Select(p => p.Datetime).Distinct().OrderByDescending(p => p).Take(2).ToArrayAsync();
-
             context.Database.ExecuteSqlCommand(GetSqlToUpdate發行張數(dd[0].ToString("yyyy-MM-dd"), dd[1].ToString("yyyy-MM-dd")));
-            context.Database.ExecuteSqlCommand(GetSqlToUpdate());
 
             s.Stop();
             Console.WriteLine($"Spend times {s.Elapsed.TotalMinutes} minutes.");
+        }
+        private static string GetSql()
+        {
+            return @$"
+  select s.* from [Stocks]  s 
+  left join (select * from [Prices] where [Datetime] = '{DateTime.Today:yyyy/MM/dd}') p on s.StockId = p.StockId
+  where  s.Status = 1 and p.Id is null
+  order by s.StockId desc";
         }
 
         [Obsolete]
@@ -69,32 +75,6 @@ namespace WebCrawler
 
             s.Stop();
             Console.WriteLine($"Spend times {s.Elapsed.TotalMinutes} minutes.");
-        }
-
-        private static string GetSql()
-        {
-            return @$"
-  select s.* from [Stocks]  s 
-  left join (select * from [Prices] where [Datetime] = '{DateTime.Today.ToString("yyyy/MM/dd")}') p on s.StockId = p.StockId
-  where  s.Status = 1 and p.Id is null
-  order by s.StockId desc";
-        }
-
-        private async Task ExecuteHistoryAsync(CnyParser parser, StockDbContext context, string stockId, string name)
-        {
-            var prices = parser.ParserHistory(stockId, name);
-
-            foreach (var price in prices)
-            {
-                var p = context.Prices.FirstOrDefault(p => p.Datetime == price.Datetime && p.StockId == stockId);
-                if (p == null)
-                {
-                    context.Prices.Add(price);
-                }
-            }
-            Console.WriteLine($"Finished: {stockId} {name}");
-
-            await context.SaveChangesAsync();
         }
 
         private async Task ExecuteLastAsync(CnyParser parser, StockDbContext context, string stockId, string name)
@@ -149,27 +129,6 @@ SELECT *
    where [Datetime] = '{firstDatetime}' and  ([發行張數] is null or [發行張數] = 0)
 
 ");
-        }
-        private Prices[] ParserHistory(string stockId, string name)
-        {
-            try
-            {
-                Console.WriteLine($"{stockId} : Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-                var prices = ParseHistoryPrice(stockId, name);
-                ParseNode(3, $"https://www.cnyes.com/twstock/Margin/{stockId}.htm", "/html/body/div[5]/div[1]/form/div[3]/div[5]/div[2]/table", prices, (htmlNode, p) => SetMargin(htmlNode, p));
-                ParseNode(5, $"https://www.cnyes.com/twstock/QFII/{stockId}.htm", "/html/body/div[5]/div[1]/form/div[3]/div[5]/div[3]/table", prices, (htmlNode, p) => SetForeign(htmlNode, p));
-                ParseNode(3, $"https://www.cnyes.com/twstock/itrust/{stockId}.htm", "/html/body/div[5]/div[1]/form/div[3]/div[5]/div[4]/table", prices, (htmlNode, p) => SetItrust(htmlNode, p));
-                ParseNode(3, $"https://www.cnyes.com/twstock/dealer/{stockId}.htm", "/html/body/div[5]/div[1]/form/div[3]/div[5]/div[4]/table", prices, (htmlNode, p) => SetDealer(htmlNode, p));
-
-                return prices;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"{stockId} : Failed: {ex.Message}");
-                Console.WriteLine($"{stockId} : Failed: {ex.StackTrace}");
-                ErrorStocks.AddOrUpdate(stockId, name, (key, oldValue) => name);
-                return new Prices[0];
-            }
         }
 
         public Prices ParserLastDay(string stockId, string name)
@@ -253,11 +212,11 @@ SELECT *
 
                 try
                 {
-                    ParseSingleNode(3, $"https://www.cnyes.com/twstock/Margin/{stockId}.htm", "/html/body/div[5]/div[1]/form/div[2]/div[5]/div[2]/table", price, (htmlNode, p) => SetMargin(htmlNode, p));
-                    ParseSingleNode(5, $"https://www.cnyes.com/twstock/QFII/{stockId}.htm", "/html/body/div[5]/div[1]/form/div[2]/div[5]/div[3]/table", price, (htmlNode, p) => SetForeign(htmlNode, p));
-                    ParseSingleNode(3, $"https://www.cnyes.com/twstock/itrust/{stockId}.htm", "/html/body/div[5]/div[1]/form/div[2]/div[5]/div[4]/table", price, (htmlNode, p) => SetItrust(htmlNode, p));
-                    ParseSingleNode(3, $"https://www.cnyes.com/twstock/dealer/{stockId}.htm", "/html/body/div[5]/div[1]/form/div[2]/div[5]/div[4]/table", price, (htmlNode, p) => SetDealer(htmlNode, p));
+                    ParseMargin(stockId, price);
                     await context.SaveChangesAsync();
+                    var sql = $"exec [usp_Update_Single_MA_And_VMA] '{stockId}', '{DateTime.Today:yyyy-MM-dd}'";
+                    context.Database.ExecuteSqlCommand(sql);
+
                     Console.WriteLine(stockId + "::" + name);
                 }
                 catch (Exception ex)
@@ -277,83 +236,6 @@ SELECT *
             ErrorStocks = new ConcurrentDictionary<string, string>();
         }
 
-        private Prices SetPrice(HtmlNode htmlNode, string stockId, string name)
-        {
-            return new Prices
-            {
-                Id = Guid.NewGuid(),
-                StockId = stockId,
-                Name = name,
-                CreatedOn = DateTime.Now,
-                Datetime = Convert.ToDateTime(htmlNode.ChildNodes[0].InnerText),
-                Open = Convert.ToDecimal(htmlNode.ChildNodes[1].InnerText),
-                High = Convert.ToDecimal(htmlNode.ChildNodes[2].InnerText),
-                Low = Convert.ToDecimal(htmlNode.ChildNodes[3].InnerText),
-                Close = Convert.ToDecimal(htmlNode.ChildNodes[4].InnerText),
-                漲跌 = Convert.ToDecimal(htmlNode.ChildNodes[5].InnerText),
-                漲跌百分比 = Convert.ToDecimal(htmlNode.ChildNodes[6].InnerText.Replace("%", "")),
-                成交量 = Convert.ToInt32(htmlNode.ChildNodes[7].InnerText.Replace(",", "")),
-                成交金額 = Convert.ToInt32(htmlNode.ChildNodes[8].InnerText.Replace(",", "")),
-                本益比 = Convert.ToDecimal(htmlNode.ChildNodes[9].InnerText)
-            }; 
-        }
-
-        private void SetMargin(HtmlNode htmlNode, Prices price)
-        {
-            price.融資買進 = Convert.ToInt32(htmlNode.ChildNodes[1].InnerText);
-            price.融資賣出 = Convert.ToInt32(htmlNode.ChildNodes[2].InnerText);
-            price.融資現償 = Convert.ToInt32(htmlNode.ChildNodes[3].InnerText);
-            price.融資餘額 = Convert.ToInt32(htmlNode.ChildNodes[4].InnerText);
-            var used = htmlNode.ChildNodes[6].InnerText.Replace("%", "");
-            price.融資使用率 = used == "-" ? 0 : Convert.ToDecimal(used);
-            price.融券賣出 = Convert.ToInt32(htmlNode.ChildNodes[7].InnerText.Replace("%", ""));
-            price.融券買進 = Convert.ToInt32(htmlNode.ChildNodes[8].InnerText.Replace(",", ""));
-            price.融券餘額 = Convert.ToInt32(htmlNode.ChildNodes[10].InnerText.Replace(",", ""));
-            price.資券相抵 = Convert.ToInt32(htmlNode.ChildNodes[12].InnerText);
-        }
-
-        private void SetForeign(HtmlNode htmlNode, Prices price)
-        {
-            price.外資買進 = Convert.ToInt32(htmlNode.ChildNodes[1].InnerText.Replace(",", ""));
-            price.外資賣出 = Convert.ToInt32(htmlNode.ChildNodes[2].InnerText.Replace(",", ""));
-            price.外資買賣超 = Convert.ToInt32(htmlNode.ChildNodes[3].InnerText.Replace(",", ""));
-            price.外資持股 = Convert.ToInt32(htmlNode.ChildNodes[4].InnerText.Replace(",", ""));
-            price.外資持股比例 = Convert.ToDecimal(htmlNode.ChildNodes[5].InnerText);
-            price.尚可投資張數 = Convert.ToInt32(htmlNode.ChildNodes[6].InnerText.Replace(",", ""));
-            price.發行張數 = Convert.ToInt32(htmlNode.ChildNodes[7].InnerText.Replace(",", ""));
-        }
-
-        private void SetItrust(HtmlNode htmlNode, Prices price)
-        {
-            price.投信買進 = Convert.ToInt32(htmlNode.ChildNodes[1].InnerText.Replace(",", ""));
-            price.投信賣出 = Convert.ToInt32(htmlNode.ChildNodes[2].InnerText.Replace(",", ""));
-            price.投信買賣超 = Convert.ToInt32(htmlNode.ChildNodes[3].InnerText.Replace(",", ""));
-        }
-
-        private void SetDealer(HtmlNode htmlNode, Prices price)
-        {
-            price.自營商買進 = Convert.ToInt32(htmlNode.ChildNodes[1].InnerText.Replace(",", ""));
-            price.自營商賣出 = Convert.ToInt32(htmlNode.ChildNodes[2].InnerText.Replace(",", ""));
-            price.自營商買賣超 = Convert.ToInt32(htmlNode.ChildNodes[3].InnerText.Replace(",", ""));
-        }
-
-        private Prices[] ParseHistoryPrice(string stockId, string name)
-        {
-            var s = Stopwatch.StartNew();
-            s.Start();
-            var rootNode = GetRootNoteByUrl($"https://www.cnyes.com/twstock/ps_historyprice/{stockId}.htm");
-            var node = rootNode.SelectSingleNode("/html/body/div[5]/div[1]/form/div[3]/div[5]/div[3]/table");
-
-            var prices = new List<Prices>();
-            for (int i = 3; i < node.ChildNodes.Count - 1; i++)
-            {
-                prices.Add(SetPrice(node.ChildNodes[i], stockId, name));
-            }
-
-            s.Stop();
-            Console.WriteLine("基本股價：" + s.Elapsed.TotalSeconds);
-            return prices.ToArray();
-        }
         public void ParseTrust(string stockId, Prices price) 
         {
             var s = Stopwatch.StartNew();
@@ -489,22 +371,6 @@ SELECT *
             Console.WriteLine($"{stockId}, {name}, {datetime}：" + s.Elapsed.TotalSeconds);
         }
 
-        private void ParseNode(int startIndex, string url, string xPzth, Prices[] prices, Action<HtmlNode, Prices> action)
-        {
-            var rootNode = GetRootNoteByUrl(url);
-            var node = rootNode.SelectSingleNode(xPzth);
-
-            for (int i = startIndex; i < node.ChildNodes.Count - 1; i++)
-            {
-                var datetime = Convert.ToDateTime(node.ChildNodes[i].ChildNodes[0].InnerText);
-                var p = prices.FirstOrDefault(p => p.Datetime == datetime);
-                if (p != null)
-                {
-                    action(node.ChildNodes[i], p);
-                }
-            }
-        }
-
         private Prices ParseSingleHistoryPrice(string stockId, string name)
         {
             var rootNode = GetRootNoteByUrl($"https://fubon-ebrokerdj.fbs.com.tw/Z/ZC/ZCX/ZCX_{stockId}.djhtm", false);
@@ -532,6 +398,7 @@ SELECT *
                 .InnerHtml;
             var costPercentNode = tmp2 == "N/A" ? 0 : Convert.ToDecimal(tmp2);
             var volume = Convert.ToInt32(rootNode.SelectSingleNode("//*[@id=\"SysJustIFRAMEDIV\"]/table/tr[2]/td[2]/table/tr/td/table[2]/tr[4]/td[8]").InnerHtml.Replace(",",""));
+            var percent = Math.Round(100 * updownValue / (closeValue - updownValue),3);
 
             return new Prices
             {
@@ -545,30 +412,11 @@ SELECT *
                 Low = Convert.ToDecimal(lowNode.InnerHtml),
                 Close = closeValue,
                 漲跌 = updownValue,
-                漲跌百分比 = updownValue / (closeValue - updownValue),
+                漲跌百分比 = percent,
                 成交量 = volume,
                 成交金額 = (int)(volume * closeValue),
                 本益比 = costPercentNode
             };
-        }
-
-        private void ParseSingleNode(int startIndex, string url, string xPath, Prices price, Action<HtmlNode, Prices> action)
-        {
-            var s = Stopwatch.StartNew();
-            s.Start();
-            var rootNode = GetRootNoteByUrl(url);
-            var node = rootNode.SelectSingleNode(xPath);
-
-            if (node != null && node.ChildNodes.Count > startIndex)
-            {
-                var datetime = Convert.ToDateTime(node.ChildNodes[startIndex].ChildNodes[0].InnerText);
-                if (price.Datetime == datetime)
-                {
-                    action(node.ChildNodes[startIndex], price);
-                }
-            }
-            s.Stop();
-            Console.WriteLine(url + "：" +s.Elapsed.TotalSeconds);
         }
     }
 }
