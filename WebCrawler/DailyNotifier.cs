5cc6c87312e6f115cf53aaa5dc43a93d9b2773fa
lester.wei <lester.wei@alliance.tw>
2021-05-02 19:00:00 +0800

    add WeekNotifier

--- WebCrawler/DailyNotifier.cs
+++ WebCrawler/DailyNotifier.cs
@@ -38,36 +38,6 @@ namespace WebCrawler
             await NotifyBotApiAsync(漲停板股票);
         }
 
-        private string Get外資投信主力買超股票(StockDbContext context)
-        {
-            var tmpPrices = context.Prices.Where(p =>
-               p.Datetime == DateTime.Today &&
-               p.Close < 150 &&
-               p.外資買賣超 > 0 &&
-               p.投信買賣超 > 0 &&
-               (p.主力買超張數 - p.主力賣超張數) > 0
-           );
-
-            var prices = from pp in tmpPrices
-                         join s in context.Stocks on pp.StockId equals s.StockId
-                         where s.Industry != "金融保險業"
-                         select pp;
-
-            var msg = new StringBuilder();
-            msg.AppendLine($"外資, 投信, 主力買超股票 : {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
-
-            var index = 1;
-            foreach (var price in prices)
-            {
-                var updown = (price.漲跌 > 0) ? "+" + price.漲跌 : price.漲跌.ToString(CultureInfo.InvariantCulture);
-                var updownPercent = (price.漲跌 > 0) ? "+" + price.漲跌百分比 : price.漲跌百分比.ToString(CultureInfo.InvariantCulture);
-                msg.AppendLine($"{index}. {price.StockId} {price.Name} {price.Close} {updown}  {updownPercent}%");
-                index++;
-            }
-
-            return msg.ToString();
-        }
-
         private string 漲停板(StockDbContext context)
         {
             var prices = context.Prices.Where(p =>
@@ -221,77 +191,6 @@ order by b.買超 desc
 
             var msg = new StringBuilder();
             msg.AppendLine($"董監買超股票 : {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
-
-            var index = 1;
-            foreach (var stock in stocks)
-            {
-                msg.AppendLine($"{index}. {stock.StockId} {stock.Name} {stock.股價} {stock.Description}");
-                index++;
-            }
-            return msg.ToString();
-        }
-
-
-
-        private async Task<string> Get投信突然加入買方Sql(StockDbContext context)
-        {
-            var datetime = DateTime.Today.ToString("yyyy-MM-dd");
-            var dd = Convert.ToDateTime(datetime);
-            var datetime2 = context.Prices.Where(p => p.StockId == "2330" && p.Datetime < dd)
-                .OrderByDescending(p => p.Datetime)
-                .Take(5)
-                .OrderBy(p => p.Datetime)
-                .Select(p => p.Datetime.ToString("yyyy-MM-dd"))
-                .FirstOrDefault();
-
-            var sql = $@"
-  WITH TOPTEN as (
-   SELECT *, ROW_NUMBER() 
-    over (
-        PARTITION BY [Name] 
-       order by [Datetime] desc
-    ) AS RowNo 
-    FROM [Prices] where [Datetime] <= '{datetime}' and [Datetime] >= '{datetime2}'
-)
-
-select 
-      s.[StockId]
-      ,s.[Name]
-      ,s.[MarketCategory]
-      ,s.[Industry]
-      ,s.[ListingOn]
-      ,s.[CreatedOn]
-      ,s.[UpdatedOn]
-      ,s.[Status]
-      ,s.[Address]
-      ,s.[Website]
-      ,s.[營收比重]
-      ,s.[股本]
-      ,s.[股價]
-      ,s.[每股淨值]
-      ,s.[每股盈餘], s.[ROE], s.[ROA]
-	  ,CAST(t1.[投信買賣超] AS nvarchar(30)) AS [Description]
-      ,股票期貨
-from [Stocks]s 
-join TOPTEN t1 on s.StockId = t1.StockId
-join TOPTEN t2 on t1.StockId = t2.StockId and t1.RowNo + 1 = t2.RowNo
-join TOPTEN t3 on t1.StockId = t3.StockId and t1.RowNo + 2 = t3.RowNo
-join TOPTEN t4 on t1.StockId = t4.StockId and t1.RowNo + 3 = t4.RowNo
-join TOPTEN t5 on t1.StockId = t5.StockId and t1.RowNo + 4 = t5.RowNo
-join TOPTEN t6 on t1.StockId = t6.StockId and t1.RowNo + 5 = t6.RowNo
-WHERE t1.RowNo=1 and 
-t1.[投信買賣超] > 0 and 
-t2.[投信買賣超] <= 0 and
-t3.[投信買賣超] <= 0 and
-t4.[投信買賣超] <= 0 and
-t5.[投信買賣超] <= 0 and
-t6.[投信買賣超] <= 0
-order by t1.[投信買賣超] desc
-";
-
-            var stocks = await context.Stocks.FromSqlRaw(sql).ToArrayAsync();
-            var msg = new StringBuilder();
-            msg.AppendLine($"投信突然加入買方 : {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
 
             var index = 1;
             foreach (var stock in stocks)
--- WebCrawler/Main.cs
+++ WebCrawler/Main.cs
@@ -64,7 +64,7 @@ namespace WebCrawler
             //await ss.ParserMarginAsync();
             //var ss =  s.ParseTrust("2330", "2017-01-01", "2019-10-29");
 
-            await RunAsync<DailyNotifier>();
+            await RunAsync<WeekNotifier>();
 
             //await RunAsync<RealtimeStockParser>();
             //Console.ReadLine();
